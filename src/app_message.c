f85d550a33bcf8d4fa525abfe7b72a8dc6875ed4
Walter B. Vaughan <wbvaughan@steelerubber.com>
2014-10-26 08:43:51 -0400

    here is the code
    yep.

---
+++ src/app_message.c
@@ -0,0 +1,107 @@
+#include <pebble.h>
+
+#define NUM_NEARBY_BUSES 5	
+	
+Window *window;
+Layer *window_layer;
+TextLayer *text_layer;
+
+static char* js_msg_recieved = "hold on..."; // default message on load
+
+static char r_nearby_datum[160];
+	
+// Key values for AppMessage Dictionary
+enum {
+	STATUS_KEY = 0,
+	DATA_KEY,
+	DEBUG_KEY
+};
+
+
+// Write message to buffer & send
+void send_ack_message(void){
+	DictionaryIterator *iter;
+	
+	app_message_outbox_begin(&iter);
+	dict_write_uint8(iter, STATUS_KEY, 0x1);
+	
+	dict_write_end(iter);
+  	app_message_outbox_send();
+}
+
+// Write message to buffer & send
+void send_nack_message(void){
+	DictionaryIterator *iter;
+	
+	app_message_outbox_begin(&iter);
+	dict_write_uint8(iter, STATUS_KEY, 0x2);
+	
+	dict_write_end(iter);
+  	app_message_outbox_send();
+}
+
+// Called when a message is received from PebbleKitJS
+static void in_received_handler(DictionaryIterator *received, void *context) {
+	Tuple *rec_debug = dict_find(received, DEBUG_KEY);
+	Tuple *rec_data = dict_find(received, DATA_KEY);
+	
+	if (rec_debug) {
+		text_layer_set_text(text_layer, rec_debug->value->cstring);
+		layer_mark_dirty(window_layer);
+	} else if (rec_data) {
+		text_layer_set_text(text_layer, rec_data->value->cstring);
+		layer_mark_dirty(window_layer);
+	}
+	
+	send_ack_message();
+}
+
+// Called when an incoming message from PebbleKitJS is dropped
+static void in_dropped_handler(AppMessageResult reason, void *context) {
+	send_nack_message();
+}
+
+// Called when PebbleKitJS does not acknowledge receipt of a message
+static void out_failed_handler(DictionaryIterator *failed, AppMessageResult reason, void *context) {
+	send_nack_message();
+}
+
+void init(void) {
+	window = window_create();
+	window_stack_push(window, true);
+	
+	// initialize the only TextLayer
+	window_layer = window_get_root_layer(window);
+	GRect bounds = layer_get_frame(window_layer);
+	text_layer = text_layer_create((GRect){ .origin = { 0, 30 }, .size = bounds.size });
+	
+	// Register AppMessage handlers
+	app_message_register_inbox_received(in_received_handler); 
+	app_message_register_inbox_dropped(in_dropped_handler); 
+	app_message_register_outbox_failed(out_failed_handler);
+		
+	app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+	
+	send_ack_message();
+	
+	// set TextLayer properties
+	text_layer_set_text(text_layer, js_msg_recieved);
+	text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_14));
+	text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+	
+	layer_add_child(window_layer, text_layer_get_layer(text_layer));
+}
+
+void deinit(void) {
+	app_message_deregister_callbacks();
+	
+	text_layer_destroy(text_layer);
+	
+	window_destroy(window);
+}
+
+int main( void ) {
+	init();
+	app_event_loop();
+	deinit();
+}
---
+++ src/js/pebble-js-app.js
@@ -0,0 +1,83 @@
+// global array holding messages to be sent
+var bus_msg_que = [];
+
+// Function to send a message to the Pebble using AppMessage API
+function sendMessage() {
+	
+	var req = new XMLHttpRequest();
+	//req.open( "GET", "http://www.nextbus.com/webkit/predsByLoc.jsp?lat=35.910092&lon=-79.05321789999999&maxDis=23000&maxNumStops=999", true );
+	req.open( "GET", "http://152.23.22.74/", true );
+	req.responseType = "document";
+	req.onload = function(e) {
+		if (req.readyState == 4 && req.status == 200) {
+			var currentNode = req.responseXML.getElementsByClassName("plainText")[0].childNodes[0].nextSibling;
+
+			var route;
+			var direction;
+			var stop;
+			var estimate;
+			var num_stops_recorded = 0;
+
+			while (currentNode && num_stops_recorded < 5) {
+				if (currentNode.className == "inBetweenRouteSpacer") {
+					route = currentNode.childNodes[1].innerHTML;
+				} else if (currentNode.className == "link normalDirLinkColor" ||
+							currentNode.className == "link altDirLinkColor") {
+					direction = currentNode.childNodes[1].childNodes[1].innerHTML;
+					stop = currentNode.childNodes[1].childNodes[3].data.trim().substr(6);
+					estimate = currentNode.childNodes[1].childNodes[7].innerHTML.split(" ")[0];
+
+					if (estimate != "Arriving") {
+						estimate = estimate + " min";
+					}
+					
+					num_stops_recorded += 1;
+					
+					// send the recorded data to pebble, newlines denoted by '$' delimiter
+					bus_msg_que.push({"1": route + "\n" +
+											direction + "\n" +
+											stop + "\n" + 
+											estimate + "\n"});
+				}
+				currentNode = currentNode.nextElementSibling;
+			}
+		} else {
+			Pebble.sendAppMessage({"2": "Error connecting to server."});
+		}
+		
+		sendMsg();
+		
+	};
+	req.send();
+	
+	// PRO TIP: If you are sending more than one message, or a complex set of messages, 
+	// it is important that you setup an ackHandler and a nackHandler and call 
+	// Pebble.sendAppMessage({ /* Message here */ }, ackHandler, nackHandler), which 
+	// will designate the ackHandler and nackHandler that will be called upon the Pebble 
+	// ack-ing or nack-ing the message you just sent. The specified nackHandler will 
+	// also be called if your message send attempt times out.
+}
+
+function sendMsg() {
+	if (bus_msg_que.length > 0) {
+		Pebble.sendAppMessage(bus_msg_que[0], sendNextMsg, sendMsg);
+	}
+}
+
+function sendNextMsg() {
+	if (bus_msg_que.length > 0) {
+		bus_msg_que.shift();
+		sendMsg();
+	}
+}
+
+
+// Called when JS is ready
+Pebble.addEventListener("ready", function(e) {
+	sendMessage();
+});												
+// Called when incoming message from the Pebble is received
+Pebble.addEventListener("appmessage",
+							function(e) {
+								
+							});
